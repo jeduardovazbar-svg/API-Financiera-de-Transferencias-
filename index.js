42b8fc71dd6a8a2cdba7805e7da1965c78900004
Lalobatimoledor <jeduardovazbar@gmail.com>
2026-07-17 07:57:03 -0600

    creando el app token

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config(); // Cargar las variables de entorno al inicio
 const express = require('express');
 const mongoose = require('mongoose'); // Importar Mongoose
-
+const appTokensito = require('./src/middlewares/appToken')
 const app = express();
 
 // Middleware
 app.use(express.json());
-
+app.use(appTokensito);
 // Importar rutas
 const usuarioRoutes = require('./src/routes/usuarioRoutes'); 
 const cuentaRoutes = require('./src/routes/cuentaRoutes'); 
@@ -28,10 +28,10 @@ mongoose.connect(uri)
     .catch((error) => console.error('❌ Error al conectar a MongoDB:', error.message));
 
 // Si quisieras probar local, descomenta esto, pero para Vercel no es necesario:
-// if (!process.env.VERCEL) {
-//     const PORT = process.env.PORT || 3000;
-//     app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
-// }
+ if (!process.env.VERCEL) {
+     const PORT = process.env.PORT || 3000;
+     app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
+ }
 
 // CRUCIAL: Siempre expórtalo en la raíz del archivo, fuera de cualquier función o promesa
 module.exports = app;
